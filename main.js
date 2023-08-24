c67cf9c82e1f89cbf0aff77e7b6d9b8cf2d2ad54
Yago Gomes <yaguamineral@gmail.com>
2023-08-24 15:08:08 -0300

    adicionado validação de formulario

--- main.js
+++ main.js
@@ -6,4 +6,29 @@ $(document).ready(function() {
     $('.menu-hamburguer').click(function(){
         $('nav').slideToggle();
     })
+    let numero = $('#telefone').val();
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder:'(00) 00000-0000'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Insira um nome válido.',
+            telefone:'Este campo é obrigatório',
+            email:'Este campo é obrigatório'
+        },
+    })
+    
 })
